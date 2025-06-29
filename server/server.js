362b8bd58b3d9c279fbc12a3d6143802013f72bc
SorayuthJapanya <sorayuthjaapanya@gmail.com>
2025-06-29 21:20:01 +0700

    Update Dockerfile to set gunicorn worker and thread options, and increase timeout to 600 seconds

--- server/server.js
+++ server/server.js
@@ -44,34 +44,14 @@ app.use(cors(corsOptions));
 
 app.use("/api/v1/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes
-app.get("/api/v1/cookies", function (req, res) {
-  // Check if cookies exist
-  if (req.cookies && Object.keys(req.cookies).length > 0) {
-    res.json({
-      message: "Cookies found",
-      cookies: req.cookies,
-      signedCookies: req.signedCookies,
-    });
-  } else {
-    res.json({
-      message: "No cookies found",
-      cookies: {},
-      signedCookies: {},
-    });
-  }
-});
-
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", uploadRoutes);
 app.use("/api/v1/species", speciesRoutes);
 app.use("/api/v1/history", historyRoutes);
 
 // Server React
-app.setTimeout(3600000); 
 
 app.listen(PORT, async () => {
   await connectDB();
   console.log("Server is running on port:", PORT);
 });
-
